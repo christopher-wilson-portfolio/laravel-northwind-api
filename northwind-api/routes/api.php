c90f33b0c63ab34ad0ce07648ca77dcc2467bda8
Christopher Wilson <cwilson@christopherwilson.biz>
2023-06-10 21:50:17 -0500

    Updated Models and Controllers

--- northwind-api/routes/api.php
+++ northwind-api/routes/api.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\CustomerDemographicController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailsController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegionController;
+use App\Http\Controllers\ShipperController;
+use App\Http\Controllers\TerritoryController;
+use App\Http\Middleware\FormatJsonResponse;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +28,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('categories', CategoryController::class);
+
+Route::resource('customers', CustomerController::class);
+
+Route::resource('customer_demographics', CustomerDemographicController::class);
+
+Route::resource('employees', EmployeeController::class);
+
+Route::resource('orders', OrderController::class);
+
+Route::resource('orders_details', OrderDetailsController::class);
+
+Route::resource('products', ProductController::class);
+
+Route::resource('regions', RegionController::class);
+
+Route::resource('shippers', ShipperController::class);
+
+Route::resource('customers', TerritoryController::class);
